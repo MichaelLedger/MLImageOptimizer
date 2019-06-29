53923aaad893f77f9fef052dcd20af5a019e9b61
MichaelLedger <MichaelLedger@163.com>
2019-06-29 14:48:35 +0800

    脚本优化，支持多种路径模式

--- replace.sh
+++ replace.sh
@@ -14,7 +14,12 @@ cd $source_dir
 echo "--- start compress png ---"
 find . -name "*.png" -print | xargs -n1
 pngquant=$path/lib/pngquant
+#&&左边的命令（命令1）返回真(即返回0，成功被执行）后，&&右边的命令（命令2）才能够被执行；换句话说，“如果这个命令执行成功&&那么执行这个命令”。||恰恰相反。
+if [[ $source_dir =~ "/" ]] ;then
 des_dir=$source_dir
+else
+des_dir="../"$source_dir
+fi
 echo "--- png_output_des_dir:"$des_dir" ---"
 [ -d $des_dir ] || mkdir -p $des_dir
 for file in `find . -name "*.png"`
@@ -26,23 +31,31 @@ done
 echo "--- start compress jpg ---"
 find . \( -name "*.jpg" -o -name "*.jpeg" \) -print | xargs -n1
 jpegoptim=$path/lib/jpegoptim
+if [[ $source_dir =~ "/" ]] ;then
 des_dir=$source_dir
+else
+des_dir="../"$source_dir
+fi
 echo "--- jpg_output_des_dir:"$des_dir" ---"
 [ -d $des_dir ] || mkdir -p $des_dir
 for file in `find . \( -name "*.jpg" -o -name "*.jpeg" \)`
 do
 original_file=$file
 file_name=`echo ${original_file#*/}`
-[[ $file_name =~ "/" ]] && echo "$file_name contains '/'" && sub_file_path=`echo ${file_name%/*}`
+[[ $file_name =~ "/" ]] && sub_file_path=`echo ${file_name%/*}`
 [ -d $des_dir"/"$sub_file_path ] || mkdir -p $des_dir"/"$sub_file_path
-[[ $file_name =~ "/" ]] || echo "$file_name does NOT contains '/'" || sub_file_path=""
+[[ $file_name =~ "/" ]] || sub_file_path=""
 echo $file_name"===="$des_dir"/"$sub_file_path
 $jpegoptim $file -m40 -o -d $des_dir"/"$sub_file_path
 done
 echo "--- start compress gif ---"
 find . -name "*.gif" -print | xargs -n1
 gifsicle=$path/lib/gifsicle
+if [[ $source_dir =~ "/" ]] ;then
 des_dir=$source_dir
+else
+des_dir="../"$source_dir
+fi
 echo "--- gif_output_des_dir:"$des_dir" ---"
 [ -d $des_dir ] || mkdir -p $des_dir
 for file in `find . -name "*.gif"`
